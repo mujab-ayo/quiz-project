2c29b5b88b6e865f1fb8afc4469e89f27e29679c
Abdulmujeeb <abdulroheemabdulmujeeb@gmail.com>
2024-08-25 01:39:58 -0700

    done with styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: "#38d9a9",
+        lightTeal: "#c3fae8",
+        darkGrey: "#495057",
+        midGrey: "#e9ecef",
+        ligthGrey: "#f1f3f5",
+      },
+    },
+  },
+  plugins: [],
+};
